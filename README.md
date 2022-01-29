1e9ab07372a1e46d784f8c6cb1a37a5479212ba6
SophieQuan <sophiequan7@gmail.com>
2022-01-29 16:41:43 -0500

    finalize simmply scheduler

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # simply-my-scheduler
+A simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## App Features
+* Current day is displayed at the top of the calendar
+* Time block is color-coded to indicate whether it is in the past, present, or future
+* Add, edit and save event to the time block
+
+# Build with
+* HTML
+* CSS 
+* jQuery
+* BOOTRAP
+* Moment.js
+
+## Web URL
+https://sophiequan.github.io/simply-my-scheduler/
+
+## Repository URL
+https://github.com/SophieQuan/simply-my-scheduler
+
+## Screenshots
+
---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+//display current day and hour
+setInterval(function(){
+    var currentDay = moment().format('[Today, ] MMMM Do, YYYY - h:mm:ss A');
+    $('#currentDay').text(currentDay);
+}, 1000);
+
+//on click save time and notess to local storage
+$(".saveBtn").on("click", function () {
+    var taskTime = $(this).siblings(".hour").text();
+    var notes = $(this).siblings("textarea").val();
+
+    localStorage.setItem(taskTime, notes);
+
+});
+
+//load task
+var loadTasks = function(){
+    $(".hour").each(function(){
+        var getHour = $(this).text();
+        var loadTaskNotes = localStorage.getItem(getHour);
+
+        $(this).siblings(".description").val(loadTaskNotes);
+
+    })
+}
+
+//change color represent current time
+var colorBlock = function(){
+    var currentHour = moment().hour();
+    // console.log(currentHour);
+    // currentHour = 15;
+
+    for (i=9; i< 18; i++){
+        var timeBlock  = $("#hour-" + i).find(".description");
+        timeBlock.removeClass("past present future");
+
+        if(currentHour > i){
+            timeBlock.addClass("past")
+        }else if (currentHour === i){
+            timeBlock.addClass("present")
+        }else{
+            timeBlock.addClass("future")
+        }
+    }
+}
+var copyright = document.getElementById('copyright');
+var getYear = document.createTextNode(new Date().getFullYear());
+
+copyright.appendChild(getYear)
+
+//run colour block function every 30 mins
+setInterval(function(){
+    colorBlock();
+}(1000 * 60) * 35)
+
+colorBlock();
+loadTasks();
+
